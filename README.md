aac07a0cc4559f200ac75d4079c7fbf6a0501fd9
Iwan Firmawan <firmawaneiwan@gmail.com>
2021-08-17 06:07:37 +0700

    Update setup env variable with dot file

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Project dengan angularjs 1
 
 ## How to switch _NODE_ENV
+```bash
+cp .env.js.example env.js
+```
 edit env.js at line 32.
 ```
 window.__env.enableDebug = //true or false
--- index.php
+++ index.php
@@ -22,10 +22,10 @@
     <script src="/public/assets/js/select2.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
+    <script src="env.js"></script>
     <script src="/apps/app.js"></script>
     <script src="/apps/controller.js"></script>
     <script src="/apps/variable.js"></script>
-    <script src="env.js"></script>
     <link rel="stylesheet" href="/public/assets/css/style.css?v=1" />
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
