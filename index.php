551de3bcf57d67cad325fb3b0b7de7738fd9bbb3
Bagus Rachmad Hartanto Prabowo <bagusracmad@gmail.com>
2021-08-18 21:26:32 +0700

    Update chart #37

--- index.php
+++ index.php
@@ -21,12 +21,16 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
+
     <script src="env.js"></script>
     <script src="/apps/app.js"></script>
     <script src="/apps/controller.js"></script>
     <script src="/apps/variable.js"></script>
     <script src="/env.js"></script>
     <link rel="stylesheet" href="/public/assets/css/style.css?v=1" />
+    <link rel="stylesheet" href="/panel/public/assets/vendors/chartjs/Chart.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
@@ -50,6 +54,8 @@
         integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
         crossorigin="anonymous"></script>
 
+       
+
 
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
@@ -67,6 +73,9 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
     <script src="/public/script.js"></script>
+    <script src="/panel/public/assets/vendors/chartjs/Chart.min.js"></script>
+    <script src="/panel/public/assets/js/pages/ui-chartjs.js"></script>
+        
 </body>
 
 </html>
